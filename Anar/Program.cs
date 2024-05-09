ba66156cc6172744c45c9644f62b108707898bb1
Nicholas J. Fiorello Jr <nfiorello@yahoo.com>
2024-05-09 17:09:43 -0400

    Use serilog

--- Anar/Program.cs
+++ Anar/Program.cs
@@ -2,13 +2,34 @@ using Anar;
 using Anar.Extensions;
 using Anar.Services;
 
-var builder = Host.CreateApplicationBuilder(args);
-// Wedge in some docker love.
-builder.Configuration.AddDockerConfiguration();
+using Serilog;
 
-builder.Services.AddGatewayClient();
-builder.Services.AddInfluxDB();
-builder.Services.AddHostedService<Worker>();
+// The down side to doing this here is that starup logging used base config.
+Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateBootstrapLogger();
 
-var host = builder.Build();
-host.Run();
+try {
+  var builder = Host.CreateApplicationBuilder(args);
+  builder.Configuration.AddDockerConfiguration();
+
+  // Now that configuration is augmented add the real logger.
+  builder.Services.AddSerilog((services, cfg) => cfg
+    .ReadFrom.Configuration(builder.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+  );
+
+  builder.Services.AddGatewayClient();
+  builder.Services.AddInfluxDB();
+  builder.Services.AddHostedService<Worker>();
+
+  var host = builder.Build();
+  host.Run();
+} catch (Exception ex) {
+  Log.Fatal(ex, "Host terminated unexpectedly");
+} finally {
+  Log.CloseAndFlush();
+}
