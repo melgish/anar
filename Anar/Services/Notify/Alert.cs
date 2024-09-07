417528ba2de6a634369c75c98fbde6fb5d200445
Nicholas J. Fiorello Jr <nfiorello@yahoo.com>
2024-09-07 10:02:18 -0400

    rename file

--- Anar/Services/Notify/Alert.cs
+++
@@ -1,6 +0,0 @@
-namespace Anar.Services.Notify;
-
-internal record Alert;
-internal sealed record SimpleAlert(string Message) : Alert;
-internal sealed record ThumbprintAlert(string Expected, string Actual) : Alert;
-internal sealed record AuthenticationAlert(string Message) : Alert;
